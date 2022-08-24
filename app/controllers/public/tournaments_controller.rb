f212ca09b42b37b6175d9ab85ef6d3b5fe8c4949
EC2 Default User <ec2-user@ip-172-31-79-189.ec2.internal>
2022-08-24 14:34:37 +0000

    8/24

--- app/controllers/public/tournaments_controller.rb
+++ app/controllers/public/tournaments_controller.rb
@@ -18,6 +18,7 @@ class Public::TournamentsController < ApplicationController
     #presentメソッドはデータが入ってるかどうかを識別するメソッド
     if (params[:prefecture_id]).present?
       @tournaments = Tournament.where(prefecture_id: params[:prefecture_id])
+      @tournament_name = Prefecture.find(params[:prefecture_id]).name
       #モデル.where(カラム名: params[:受け取る名前＊カラム名だとわかりやすい])
       #whereメソッドは指定した条件に当てはまるデータを全て取得してくれる
     elsif (params[:category_id]).present?
@@ -26,9 +27,7 @@ class Public::TournamentsController < ApplicationController
       @tournaments = Tournament.all
     end
 
-    if (params[:prefecture_name]).present?
-      @tournament_name = params[:prefecture_name]
-    elsif (params[:category_name]).present?
+    if (params[:category_name]).present?
       @tournament_name = params[:category_name]
     end
     @prefectures = Prefecture.all
